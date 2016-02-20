34e5cddb59c4c4851ede29c644da07fe897e3506
Joseph Blodgett <joseph.blodgett@gmail.com>
2016-02-19 23:54:04 -0700

    Final version

--- mapper.py
+++ mapper.py
@@ -10,13 +10,11 @@ import sys
 import re
 
 def getStopWords(filePath):
-	stoppattern = re.compile("'([a-zA-Z])'")
+	words = "'i','me','my','myself','we','our','ours','ourselves','you','your','yours','yourself','yourselves','he','him','his','himself','she','her','hers','herself','it','its','itself','they','them','their','theirs','themselves','what','which','who','whom','this','that','these','those','am','is','are','was','were','be','been','being','have','has','had','having','do','does','did','doing','a','an','the','and','but','if','or','because','as','until','while','of','at','by','for','with','about','against','between','into','through','during','before','after','above','below','to','from','up','down','in','out','on','off','over','under','again','further','then','once','here','there','when','where','why','how','all','any','both','each','few','more','most','other','some','such','no','nor','not','only','own','same','so','than','too','very','s','t','can','will','just','don','should','now'"
+	stoppattern = re.compile("'([a-zA-Z]*)'")
 	stopWords = []
-	with open(filePath, 'r') as f:
-		for line in f:
-			for word in stoppattern.findall(line):
-				stopWords.append(word)
-
+	for word in stoppattern.findall(words):
+		stopWords.append(word)
 	toReturn = set(stopWords)
 	return toReturn
 
