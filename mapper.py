8d55a4a60ce8a5a4300a524adc619a30b9ed1393
Joseph Blodgett <joseph.blodgett@gmail.com>
2016-02-19 22:46:16 -0700

    Stop words

--- mapper.py
+++ mapper.py
@@ -9,13 +9,28 @@
 import sys
 import re
 
+def getStopWords(filePath):
+	stoppattern = re.compile("'([a-zA-Z])'")
+	stopWords = []
+	with open(filePath, 'r') as f:
+		for line in f:
+			for word in stoppattern.findall(line):
+				stopWords.append(word)
+
+	toReturn = set(stopWords)
+	return toReturn
+
 def main(argv):
+
+	stopWords = getStopWords("stop-words")
 	line = sys.stdin.readline()
 	pattern = re.compile("[a-zA-Z][a-zA-Z0-9]*")
 	try:
 		while line:
 			for word in pattern.findall(line):
-				print(word.lower() + "\t" + "1")
+				#check for stop words
+				if not word in stopWords:
+					print(word.lower() + "\t" + "1")
 			line =  sys.stdin.readline()
 	except "end of file":
 		return None
